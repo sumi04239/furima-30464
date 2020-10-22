7e8f5de310c7c004299c4b096dd3ef6027bbbe47
Sumi04239 <rn5txe158@gmail.com>
2020-10-22 14:29:14 +0900

    validates修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do      
+    validates :nickname
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Last name full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"First name full-width characters."}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Last name kana full-width katakana characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana full-width katakana characters."}
+    validates :birth_date
+    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Password Include both letters and numbers"},
+                         length: { minimum: 6 }
+    validates :email, uniqueness: true
+  end
 end
