6b8db9d8c37f213e34e38eda813156fdfc7e368c
Sumi04239 <rn5txe158@gmail.com>
2020-10-24 16:25:25 +0900

    テストコード等修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,6 +69,31 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than 9,999,999')
       end
+      it 'category_idが0の時は出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1" )
+      end
+      it 'condition_idが0の時は出品できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it 'post_payer_idが0の時は出品できない' do
+        @item.post_payer_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Post payer must be other than 1")
+      end
+      it 'prefecture_idが0の時は出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'heading_time_idが0の時は出品できない' do
+        @item.heading_time_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Heading time must be other than 1")
+      end
     end
   end
 end
