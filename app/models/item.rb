6c3b669300b0f48c908e55c50d8c6b766dc2ffc4
Sumi04239 <rn5txe158@gmail.com>
2020-10-24 13:41:05 +0900

    itemテストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,21 +7,22 @@ class Item < ApplicationRecord
   belongs_to_active_hash :heading_time
 
   has_one_attached :image
+  belongs_to :user
 
-  validates :category_id, :condeition_id, :post_payer_id,
+  validates :category_id, :condition_id, :post_payer_id,
             :prefecture_id, :heading_time_id, numericality: { other_than: 1 }
 
   with_options presence: true do
-    validates :goods_name
-    validates :description
+    validates :goods_name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
     validates :category_id
-    validates :condeition_id
+    validates :condition_id
     validates :post_payer_id
     validates :prefecture_id
     validates :heading_time_id
-    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Password Include both letters and numbers"},
+    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Price Half-width number"},
                       numericality: { only_integer: true, greater_than: 300, message: "must be greater than 300"},
-                      numericality: { only_integer: true, ,less_than: 9999999, message: "must be less than 9,999,999"}
+                      numericality: { only_integer: true, less_than: 9999999, message: "must be less than 9,999,999"}
 
     validates :image
     
