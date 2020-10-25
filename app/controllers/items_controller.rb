e8500dbec8588475f5c654035a73d8b624050d87
Sumi04239 <rn5txe158@gmail.com>
2020-10-25 21:27:07 +0900

    戻るボタンの実装とbefore_action実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,15 +19,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -39,4 +37,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:goods_name, :description, :category_id, :condition_id, :post_payer_id, :prefecture_id, :heading_time_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
